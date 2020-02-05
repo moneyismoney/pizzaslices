a7eeb32414fca3424b830b68c89e0d2cb677420f
Oleksandr Tymchenko <moneyismoney@gmail.com>
2020-02-05 20:26:39 +0200

    added read

--- venv/optimize_pizza.py
+++ venv/optimize_pizza.py
@@ -2,25 +2,48 @@
 
 import os
 import sys
+import time
+
+
+def iterate_till_win(_list_of_pizzas, _slices_maximum):
+    if _slices_maximum < 0:
+        return 0
+
+
+def delta(_list_of_pizzas, _slices_maximum):
+    return _list_of_pizzas.count() - _slices_maximum
+
 
 print('Script works!')
 print(os.environ['HOME'])
 
-input_filename = 'c_medium.in';
+# input_filename = 'c_medium.in';
+input_filename = 'e_also_big.in';
 
-fptr = open(os.environ['HOME']+'/'+input_filename, 'r')
+fptr = open(os.environ['HOME'] + '/' + input_filename, 'r')
 
-for line in fptr:
-    print(line)
-    print('\n')
-
-
-
-# 1. Вичитати вхідний файл
+# 1.[Dmytro] Вичитати вхідний файл
 # Вхідний параметр: файл на диску з назвою b_small.in
 # Вихідний параметр1: Масив(список чи кортеж) input_data[], де перший елемент - перший рядок, другий елемент - другий рядок тощо
+# на майбутнє переробити на numpy
+start_time = time.time()
+with open(input_filename) as f:
+    slices, pizza = f.readline().split()
+    input_data = f.readline()
+    input_data = list(map(int, input_data.split()))
+    print(slices, pizza, input_data)
 
 
+    # for i, line in enumerate(f):
+    #     if i == 0:
+    #         slices_maximum = int(line.split()[0])
+    #         number_of_lines = int(line.split()[1])
+    #     else:
+    #         slice_of_type = list(int(number_of_slices) for number_of_slices in line.split())
+    #
+    # print(slices_maximum, number_of_lines, slice_of_type)
+
+print("--- %s seconds ---" % (time.time() - start_time))
 # 2. Зробити цикл, який проходить по всім елементам input_data[]
 
 # 3. В циклі
@@ -33,11 +56,18 @@ for line in fptr:
 #       з slices_maximum. Важливо: міряти серверний час
 #   4.2 Допоміжна функція рахує суму всіх елементів(def delta()) і віднімає slices_maximum
 
+# while True:
+    # if sum(input_data) <= slices:
+    #     break
+    # input_data.pop(-1)
+
+# 5.[Oleh] Згенерувати файл результату
+# fptr = open(os.environ['HOME'], 'w')
+
+# fptr.write(str(result) + '\n')
+
+# fptr.close()
 
 
-# 5. Згенерувати файл результату
-#fptr = open(os.environ['HOME'], 'w')
 
-#fptr.write(str(result) + '\n')
 
-#fptr.close()
